fb4110115fafe92d31ddb8d9b9510a7065aefa3d
brian.orwe <brian.orwe@gmail.com>
2023-08-27 18:07:39 +0300

    First commit start

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+  tea "github.com/charmbracelet/bubbletea"
+  wakatime_cli "github.com/wakatime/wakatime-cli"
+  lipgloss "github.com/charmbracelet/lipgloss"
+)
+
+func main(){
+  println("Yolo")
+}
